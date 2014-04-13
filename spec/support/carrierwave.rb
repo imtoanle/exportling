53a4a7acde9d4a2fcb897dfdd8750c6989364ac2
David Reece <dv.reece@gmail.com>
2014-04-13 22:52:30 +0930

    Ensure specs remove exported files

---
+++ spec/support/carrierwave.rb
@@ -0,0 +1,14 @@
+require 'fileutils'
+
+# Set the export storage directory to a specific test one
+Exportling.base_storage_directory = 'exportling_spec_directory'
+
+# Make sure we remove all uploaded files
+RSpec.configure do |config|
+  config.before(:suite) do
+    FileUtils.rm_rf("#{Rails.root}/#{Exportling.base_storage_directory}")
+  end
+  config.after(:suite) do
+    FileUtils.rm_rf("#{Rails.root}/#{Exportling.base_storage_directory}")
+  end
+end
