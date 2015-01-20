e0323e9c298f332cd6f4ba6d2e5410646766f31b
melliott <mcelliott80@gmail.com>
2015-01-21 10:16:30 +1100

    work around for forbidden attribute
    PR updates
    
    update spec

--- app/queries/exportling/concerns/exporter_default_query.rb
+++ app/queries/exportling/concerns/exporter_default_query.rb
@@ -4,9 +4,10 @@ module Exportling
 
     class KeyMissingError       < StandardError; end
     class RelationMissingError  < StandardError; end
+    class AllowedAttributesMissingError < StandardError; end
 
     module ClassMethods
-      attr_reader :key, :relation_klass
+      attr_reader :key, :relation_klass, :allowed_finder_attributes
 
       def query_options_key(key)
         @key = key
@@ -15,10 +16,14 @@ module Exportling
       def relation_class(klass)
         @relation_klass = klass
       end
+
+      def allowed_finder_attrs(allowed)
+        @allowed_finder_attributes = allowed
+      end
     end
 
     included do
-      delegate :key, :relation_klass, to: :class
+      delegate :key, :relation_klass, :allowed_finder_attributes, to: :class
 
       # Default behaviour for an exporter query object
       def initialize(options, owner)
@@ -30,7 +35,7 @@ module Exportling
       def find_each(&block)
         query_options = @options[key]
         if query_options.present?
-          relation_klass.where(query_options).find_each(&block)
+          relation_klass.where(query_options.permit(*allowed_finder_attributes)).find_each(&block)
         end
       end
 
@@ -39,6 +44,16 @@ module Exportling
       def validate_initial_state
         validate_key_set
         validate_relation_set
+        validate_allowed_attrs_set
+      end
+
+      def validate_allowed_attrs_set
+        return unless allowed_finder_attributes.blank?
+
+        error_message = 'Pass an array to allowed_finder_attrs to '\
+                  'define which attributes may be used to find '\
+                  'the model to export'
+        fail AllowedAttributesMissingError, error_message
       end
 
       def validate_key_set
